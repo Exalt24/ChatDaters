0c61bc30aa02bef6406ef19189be1297372a1c82
Daniel Alexis Cruz <dcruz@up.edu.ph>
2024-07-22 01:46:24 +0800

    Add existing project files to Git

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
-  host = "localhost:3000"
+  host = "https://chatdaters.onrender.com"
   config.action_mailer.default_url_options = { host: host }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
